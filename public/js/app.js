47d2471ad440551456e094039f00d009358e3da5
Walter Sosa <sosa.tizon@gmail.com>
2016-11-21 22:20:11 -0300

    Listado Ajax

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,21 @@ $('#find-unidad').keyup(function(evt) {
   var criterio = $('#criterio').val();
   var data = 'texto=' +valor+'&criterio='+criterio
   $.post('/unidades', data, function(res) {
-    $("#tabla-unidades").append('<tr><td>'+res.marca+'</td><td>'+ res.nro_motor+'</td><td>'+res.matricula+'</td><td>'+res.anio+'</td></tr>')
+     var obj = $('#resultado')
+     var elemento = '<table class="table table-striped table-condensed">'
+     elemento += `<tr>
+                <th>Id.Unidad</th>
+                <th>Marca</th>
+                <th>Nro.Motor</th>
+                <th>Matrícula</th>
+                <th>Año</th>
+                </tr>`
+                
+      res.unidades.forEach(function(un) {
+        elemento += `<tr><td>${un.id_unidad}</td><td>${un.marca}</td><td>${un.nro_motor}</td><td>${un.matricula}</td><td>${un.anio}</td></tr>`
+      })
+      elemento += `</table>`
+      obj.html(elemento)
   }, 'json')
+        
 })
